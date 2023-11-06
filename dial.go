915192bf057b1477603d8f2b22c309c51b6ebfd0
mikhaillav <mikhaillavelin@gmail.com>
2023-11-06 20:44:11 +0300

    change package name

--- dial.go
+++ dial.go
@@ -10,7 +10,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/sandertv/go-raknet/internal/message"
+	"github.com/OpenFarLands/go-raknet/internal/message"
 )
 
 // UpstreamDialer is an interface for anything compatible with net.Dialer.
--- example_listen_test.go
+++ example_listen_test.go
@@ -1,7 +1,7 @@
 package raknet_test
 
 import (
-	"github.com/sandertv/go-raknet"
+	"github.com/OpenFarLands/go-raknet"
 )
 
 func ExampleListen() {
